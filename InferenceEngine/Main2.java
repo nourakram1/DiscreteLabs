1bc20bac5733e3726c0adf30d9d9c482c2508d17
Mina Nabil <153772413+Mina-Nabil2004@users.noreply.github.com>
2024-11-03 14:57:11 +0200

    Update Main2.java

--- InferenceEngine/Main2.java
+++ InferenceEngine/Main2.java
@@ -1,5 +1,10 @@
 public class Main2 {
     public static void main(String[] args) {
-
+        InferenceEngine Engine = new InferenceEngine();
+        Engine.addRule(new InferenceRule("Modus ponens", new Expression("P>Q"),new Expression("P"),new Expression("Q")));
+        Engine.addRule(new InferenceRule("Modus tollens", new Expression("P>Q"),new Expression("~Q"),new Expression("~P")));
+        Engine.addRule(new InferenceRule("Hypothetical syllogism", new Expression("P>Q"),new Expression("Q>R"),new Expression("P>R")));
+        Engine.addRule(new InferenceRule("Disjunctive syllogism", new Expression("PvQ"),new Expression("~P"),new Expression("Q")));
+        Engine.addRule(new InferenceRule("Resolution", new Expression("PvQ"),new Expression("~PvR"),new Expression("QvR")));
     }
 }
