7c0ec76e0ad785ebe552ef1c5e517b6115f1d977
Mina Nabil <153772413+Mina-Nabil2004@users.noreply.github.com>
2024-11-03 15:14:54 +0200

    Update Main2.java

--- InferenceEngine/Main2.java
+++ InferenceEngine/Main2.java
@@ -8,13 +8,18 @@ public class Main2 {
         Engine.addRule(new InferenceRule("Hypothetical syllogism", new Expression("P>Q"),new Expression("Q>R"),new Expression("P>R")));
         Engine.addRule(new InferenceRule("Disjunctive syllogism", new Expression("PvQ"),new Expression("~P"),new Expression("Q")));
         Engine.addRule(new InferenceRule("Resolution", new Expression("PvQ"),new Expression("~PvR"),new Expression("QvR")));
-        
+
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Expression: ");
         Engine.addExpression(new Expression(sc.nextLine()));
         System.out.print("Enter Second Expression: ");
         Engine.addExpression(new Expression(sc.nextLine()));
-        
-        System.out.println(Engine.applyRules().getRepresentation());
+
+        Expression Inference = Engine.applyRules();
+
+        if(Inference != null)
+            System.out.println(Inference.getRepresentation());
+        else
+            System.out.println("Expressions cannot be inferred");
     }
 }
