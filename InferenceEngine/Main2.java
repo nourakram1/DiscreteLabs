720f7509c246ab4a003be847d0063134a912f0e7
Mina Nabil <153772413+Mina-Nabil2004@users.noreply.github.com>
2024-11-05 17:31:11 +0200

    Update Main2.java

--- InferenceEngine/Main2.java
+++ InferenceEngine/Main2.java
@@ -3,11 +3,11 @@ import java.util.Scanner;
 public class Main2 {
     public static void main(String[] args) {
         InferenceEngine Engine = new InferenceEngine();
-        Engine.addRule(new InferenceRule("Modus ponens", new Expression("P>Q"),new Expression("P"),new Expression("Q")));
-        Engine.addRule(new InferenceRule("Modus tollens", new Expression("P>Q"),new Expression("~Q"),new Expression("~P")));
-        Engine.addRule(new InferenceRule("Hypothetical syllogism", new Expression("P>Q"),new Expression("Q>R"),new Expression("P>R")));
-        Engine.addRule(new InferenceRule("Disjunctive syllogism", new Expression("PvQ"),new Expression("~P"),new Expression("Q")));
-        Engine.addRule(new InferenceRule("Resolution", new Expression("PvQ"),new Expression("~PvR"),new Expression("QvR")));
+        Engine.addRule(new ModusPonens());
+        Engine.addRule(new ModusTollens());
+        Engine.addRule(new HypotheticalSyllogism());
+        Engine.addRule(new DisjunctiveSyllogism());
+        Engine.addRule(new Resolution());
 
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter First Expression: ");
