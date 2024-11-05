d1f88b7f61f30c40c7ce850a1a42eb5e366245dd
Mina Nabil <153772413+Mina-Nabil2004@users.noreply.github.com>
2024-11-05 23:08:51 +0200

    Delete scr/InferenceRules directory

--- scr/InferenceRules/InferenceRule.java
+++
@@ -1,9 +0,0 @@
-package InferenceRules;
-
-import LogicalExpressionSolver.Expression;
-
-public interface InferenceRule {
-    boolean matches(Expression exp1, Expression exp2);
-    Expression apply(Expression exp1, Expression exp2);
-    String getName();
-}
